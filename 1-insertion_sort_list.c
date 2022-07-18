9abee6b7b02755e57a8b7ea3af5f2bc4bbafd120
JustineM98 <justinemugishe98@gmail.com>
2022-07-18 20:57:46 +0200

    Sorting_algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,59 +1,58 @@
 #include "sort.h"
-
 /**
- * insertion_sort_list - sorts list with insertion sort
- * @list: input list to sort
+ * insertion_sort_list - sort a dlist via insertion sort
+ * @list: the list to sort
+ *
+ *
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp, *pn, *nxt;
+	/* declarations */
+	listint_t *a, *b;
 
-	if (list && *list && (*list)->next)
+	/* check for bad things */
+	if (!(list && *list && (*list)->next))
+		return;
+
+	/* set up for some insertion sort & swap */
+	a = (*list)->next;
+
+	while (a)
 	{
-		temp = (*list)->next;
-		pn = temp->prev;
-		nxt = temp->next;
-
-		while (temp)
+		b = a;
+		a = a->next;
+		while (b && b->prev)
 		{
-			if (temp->n < pn->n)
+			if (b->prev->n > b->n)
 			{
-				swap_list(pn, temp, nxt, list);
+				swapper(b->prev, b);
+				if (!b->prev)
+					*list = b;
+				/* cast to const for print func */
+				print_list((const listint_t *)*list);
 			}
-			temp = nxt;
-			if (temp)
-			{
-				nxt = temp->next;
-				pn = temp->prev;
-			}
+			else
+				b = b->prev;
 		}
 	}
+
 }
 
-/**
- * swap_list - swaps the position of two nodes in a linked list
- * @pn: previous node
- * @temp: temp
- * @nxt: next node
- * @h: head of list
- */
-void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t **h)
-{
 
-	while (pn && temp->n < pn->n)
-	{
-		if (nxt)
-			nxt->prev = pn;
-		temp->next = pn;
-		temp->prev = pn->prev;
-		if (pn->prev)
-			(pn->prev)->next = temp;
-		pn->prev = temp;
-		pn->next = nxt;
-		nxt = pn;
-		pn = temp->prev;
-		if ((*h)->prev)
-			*h = (*h)->prev;
-		print_list(*h);
-	}
+/**
+ * swapper - a function to help swap 2 nodes in a dlist
+ * @a: one node
+ * @b: the other node
+ *
+ */
+void swapper(listint_t *a, listint_t *b)
+{
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
 }
